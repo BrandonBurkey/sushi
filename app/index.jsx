576404480e0e7dbaa39a909e29f0de2c7f829fed
BrandonBurkey <brandonlburke@gmail.com>
2024-09-06 20:20:35 +0100

    Navigation from index to home

--- app/index.jsx
+++ app/index.jsx
@@ -30,16 +30,14 @@ export default function Index() {
       <ImageBackground source={images.japanesebg} className="w-full h-full" resizeMode="contain">
         <Image source={images.chopstix} resizeMethod="contain" className="w-[550] h-[775] -top-[100] absolute" />
         <SafeAreaView className="flex-1">
-          {/* Top SUSHIMAN */}
           <View className="flex-1 justify-start mt-4 ml-8">
             <Text className="font-neothic text-2xl text-white-100">SUSHIMAN</Text>
           </View>
-          {/* Bottom SUSHIMAN */}
           <View className="flex-1 justify-end ml-8 mr-8">
             <Text className="font-neothic text-4xl text-white-100">THE TASTE OF JAPANESE FOOD</Text>
             <Text className="text-base text-white-100 mt-6">Feel the taste of most populars Japanese foods from anywhere and anytime</Text>
             <View className="mt-6">
-              <PrettyButton title="Get Started"/>
+              <PrettyButton onPress={() => router.push('/home')} title="Get Started"/>
             </View>
           </View>
         </SafeAreaView>
--- components/PrettyButton.jsx
+++ components/PrettyButton.jsx
@@ -2,12 +2,19 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import icons from "../constants/icons"
 
-const PrettyButton = ({ title }) => {
+const PrettyButton = ({ title, onPress }) => {
   return (
-    <TouchableOpacity className="w-full h-16 bg-white-20 rounded-full items-center justify-center">
+    <TouchableOpacity
+      className="w-full h-16 bg-white-20 rounded-full items-center justify-center"
+      onPress={onPress}
+    >
       <View className="flex-row justify-center items-center">
        <Text className="text-white-100 text-base">{title}</Text>
-       <Image source={icons.arrow} className="h-4 w-4 ml-2" resizeMethod='contain'/>
+       <Image
+        source={icons.arrow}
+        className="h-4 w-4 ml-2"
+        resizeMethod='contain'
+      />
       </View>
     </TouchableOpacity>
   )
